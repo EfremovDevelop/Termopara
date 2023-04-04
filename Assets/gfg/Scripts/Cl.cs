c21508d701bbc60567bedb866c96d3653bb02d25
Adisonfer <sasha_efremov123@mail.ru>
2023-04-05 00:58:42 +0300

    f

---
+++ Assets/gfg/Scripts/Cl.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+public class Cl : MonoBehaviour, IPointerClickHandler
+{
+    //bool isPacked = true;
+    private Animator anim; // 
+
+    public Termopara Parent; // Это термоара  
+    
+    public void OnPointerClick(PointerEventData eventData) //получаем события нажатия
+    {
+       
+        if (!Parent.isPacked) // если не открыта термопара
+        {
+            anim.Play("Open1"); // открываем, то есть включаем анимацию
+            Debug.Log("Привет, я открылся!"); // пишем в консоли, что открыли для удобства
+            Parent.isPacked = true; // записываем в свойство термопары, что Термопара открыта
+        }
+        else // иначе (если открыта термпопара
+        {
+            anim.Play("Close"); // закрываем термопару  
+            Debug.Log("Привет, я закрылся!"); // пишем в консоли, что закрыли для удобства
+            Parent.isPacked = false; // записываем в свойство термопары, что Термопара закрыта 
+        }
+    }
+
+    void Start()
+    {
+        anim = GetComponent<Animator>(); // получаем компонент Animator 
+    }
+
+    //Update is called once per frame
+    void Update()
+    {
+
+    }  
+}
